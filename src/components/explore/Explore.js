8fd6e4aa57bacbd0b0dfdc4eb8ceed5f2ae79496
Shaina <shainabird21@gmail.com>
2023-05-06 16:47:10 -0400

    map in place, micro changes to css

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -1,13 +1,16 @@
 import { Map } from "./Map"
+import "./Explore.css"
 
 export const Explore = () => {
 
 
   return (
-    <div className="explore">
+    <div className="explore-container">
+    <div id="map">
       <div className="map-container">
         <Map/>
       </div>
+    </div>
     </div>
   )
 }
--- src/components/explore/Map.js
+++ src/components/explore/Map.js
@@ -18,12 +18,12 @@ export const Map = () => {
     // )
 
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[35.045761, -85.308289]} zoom={10} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[35.045761, -85.308289]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
